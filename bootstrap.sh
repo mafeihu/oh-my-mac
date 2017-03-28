988b174687a6cc5f41473a9fd980fcbdd8a3449c
lemon <lemon@lemons-MacBook-Pro.local>
2017-03-28 18:07:01 +0800

    add oh-my-zsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,11 +3,19 @@
 # Gatekeeper: https://support.apple.com/zh-cn/HT202491
 sudo spctl --master-disable
 
-#
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# Initialize
+export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed '/env zsh/d')"
+
+sed 's/^# \(HIST_STAMPS\).*/\1="yyyy-mm-dd"/' ~/.zshrc > ~/.zshrc-omztemp
+mv -f ~/.zshrc-omztemp ~/.zshrc
+
+sed 's/^# \(.*en_US.UTF-8\)/\1/' ~/.zshrc > ~/.zshrc-omztemp
+mv -f ~/.zshrc-omztemp ~/.zshrc
+
+echo "export HOMEBREW_BOTTLE_DOMAIN=\"$HOMEBREW_BOTTLE_DOMAIN\"" |  tee -a ~/.zshrc
 
 # Homebrew
-export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-brew bundle
