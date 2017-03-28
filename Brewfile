e3648564b06f87b8112a151395b30a5d65c34793
guxiaobai <sikuan.gu@gmail.com>
2017-03-28 13:54:19 +0800

    command brew bundle

---
+++ Brewfile
@@ -0,0 +1,14 @@
+cask_args appdir: '/Applications'
+tap 'caskroom/cask'
+cask 'iterm2'
+
+# Software
+cask 'qq'
+cask 'electronic-wechat'
+cask 'aliwangwang'
+cask 'baiducloud'
+cask 'baiduinput'
+cask 'neteasemusic'
+cask 'thunder'
+cask 'the-unarchiver'
+cask 'libreoffice'
--- bootstrap.sh
+++ bootstrap.sh
@@ -3,28 +3,8 @@
 # Gatekeeper: https://support.apple.com/zh-cn/HT202491
 sudo spctl --master-disable
 
-# initialize
-export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
-FISH=$(grep /fish$ /etc/shells | wc -l)
-FISHERMAN=~/.config/fish/functions/fisher.fish
-
 # Homebrew
+export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew tap caskroom/cask
-brew cask install iterm2
 
-# fish shell
-if [ ! $FISH -ge 1 ]; then
-  brew install fish
-  echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
-fi
-
-# fisherman
-if [ ! -f $FISHERMAN ]; then
-  curl -Lo $FISHERMAN  --create-dirs git.io/fisher
-fi
-
-# profile
-echo "# Homebrew Bottles" | tee -a ~/.config/fish/config.fish  ~/.bash_profile
-echo "set -gx HOMEBREW_BOTTLE_DOMAIN $HOMEBREW_BOTTLE_DOMAIN" | tee -a ~/.config/fish/config.fish
-echo "export HOMEBREW_BOTTLE_DOMAIN=\"$HOMEBREW_BOTTLE_DOMAIN\"" |  tee -a ~/.bash_profile
+brew bundle
